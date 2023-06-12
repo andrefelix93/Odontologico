019a99ce7cd3f6ca0f681c29dbe01cd89ab8783f
André Eve Felix da Silva <andre_felix1993@hotmail.com>
2023-06-12 17:06:40 -0300

    Criação do model Cadastro e migrations feitas.

--- sistema_odontologico/acesso/models.py
+++ sistema_odontologico/acesso/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(unique=True, max_length=255)
+    username = models.CharField(unique=True, max_length=50)
+    password = models.CharField(max_length=255)
+    password2 = models.CharField(max_length=255)
+       
+    def __str__(self):
+        return self.username
