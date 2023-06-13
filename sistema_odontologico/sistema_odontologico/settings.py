39f37185de56d4caf00fe2f6d956205e423e7062
André Eve Felix da Silva <andre_felix1993@hotmail.com>
2023-06-13 15:10:32 -0300

    Baixando e configurando o widget tweaks.

--- sistema_odontologico/sistema_odontologico/settings.py
+++ sistema_odontologico/sistema_odontologico/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'acesso'
+    'acesso',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
