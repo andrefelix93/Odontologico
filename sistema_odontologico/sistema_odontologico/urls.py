2ad1a841f1d62ec25ffc9ab6bd46e21d2c71c67b
André Felix <dev.andresilva@gmail.com>
2023-06-15 10:50:45 -0300

    Organizando a URL.

--- sistema_odontologico/sistema_odontologico/urls.py
+++ sistema_odontologico/sistema_odontologico/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('acesso.urls')),
     path('home/', views.home, name="home"),
+    path('', include('acesso.urls')),
+
 ]
